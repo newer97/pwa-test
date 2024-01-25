72bc6284836d2c422b42ea08a0e2c175f5b263fd
naser1945 <naser1945.2@gmail.com>
2024-01-25 16:55:44 +0300

    pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,24 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>t3 pwa test</title>
+        <meta name="description" content="t3 pwa test" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
